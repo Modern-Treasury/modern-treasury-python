bd942c1829937f6fb7cb8740070f151e59a7f918
mtengbot <eng.bot@moderntreasury.com>
2026-08-08 00:06:00 +0000

    chore: regenerate SDKs from 6bc00c2004b84d961ea739b3e30f3dbc8360f9ef
    Stainless-Generated-From: e2a0cf81e19f0622282f5611cf32bc17002a4366

--- src/modern_treasury/types/return_object.py
+++ src/modern_treasury/types/return_object.py
@@ -162,6 +162,7 @@ class ReferenceNumber(BaseModel):
         "western_alliance_payment_id",
         "western_alliance_transaction_id",
         "western_alliance_wire_confirmation_number",
+        "wise_transfer_id",
     ]
     """The type of the reference number. Referring to the vendor payment id."""
 
