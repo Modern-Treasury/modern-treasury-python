e3b145b4da05fe2189c46e2cb079ac2664cf5bdc
mtengbot <eng.bot@moderntreasury.com>
2026-08-26 20:02:15 +0000

    seal

--- src/modern_treasury/types/payment_order_update_params.py
+++ src/modern_treasury/types/payment_order_update_params.py
@@ -139,10 +139,9 @@ class PaymentOrderUpdateParams(TypedDict, total=False):
     originating_party_name: Optional[str]
     """
     If present, this will replace your default company name on receiver's bank
-    statement. For ACH, only the first 16 characters of this string will be used.
-    This must be a name the account holder legally operates under, such as a DBA. Do
-    not use it to originate on behalf of another person or business, including an
-    end customer.
+    statement. This field can only be used for ACH payments currently. For ACH, only
+    the first 16 characters of this string will be used. Any additional characters
+    will be truncated.
     """
 
     priority: Literal["high", "normal"]
