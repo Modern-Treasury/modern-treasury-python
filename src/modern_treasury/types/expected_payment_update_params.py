b0e491f8852fdb197de191481ad711f6aec9e589
mtengbot <eng.bot@moderntreasury.com>
2026-09-24 16:02:17 +0000

    Build SDK
    Stainless-Generated-From: c4b1a00df3bfd631dcb4908e6940ec7a0143f05e

--- src/modern_treasury/types/expected_payment_update_params.py
+++ src/modern_treasury/types/expected_payment_update_params.py
@@ -94,7 +94,7 @@ class ExpectedPaymentUpdateParams(TypedDict, total=False):
     reconciliation_groups: Optional[object]
     """The reconciliation groups you have for this payment."""
 
-    reconciliation_rule_variables: Optional[Iterable[ReconciliationRuleParam]]
+    reconciliation_rule_variables: Iterable[ReconciliationRuleParam]
     """An array of reconciliation rule variables for this payment."""
 
     remittance_information: Optional[str]
